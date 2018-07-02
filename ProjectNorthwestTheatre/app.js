bc6f68e00aa1668ba8c2601b06b9aee2f8d8e96d
Saivarun Illendula <s530464@nwmissouri.edu>
2018-07-01 19:57:34 -0500

    Added Modal for add new admin

--- ProjectNorthwestTheatre/app.js
+++ ProjectNorthwestTheatre/app.js
@@ -16,12 +16,13 @@ var config = require('./config/config')
 var session = require('express-session')
 var mongoose = require('mongoose')
 var adminModel = require('./models/Admin.model')
+var cors = require('cors')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'frontend'));
 app.set('view engine', 'ejs');
 
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- ProjectNorthwestTheatre/frontend/src/components/Admin/AdminDashboard.vue
+++ ProjectNorthwestTheatre/frontend/src/components/Admin/AdminDashboard.vue
@@ -19,38 +19,38 @@
           </div>
           <!-- Modal body -->
           <div class="modal-body">
-            <form>
+            <form @submit.prevent="addshow" id="addshow">
               <div class="form-group row">
                 <label class="col-sm-4 form-label">Show Name:</label>
-                <input class="col-sm-7 form-control" type="text" id="showname">
+                <input class="col-sm-7 form-control" type="text" id="showname" v-model="formdata.ShowTitle" required>
               </div>
               <div class="form-group row">
                 <label class="col-sm-4 form-label">Playwright:</label>
-                <input class="col-sm-7 form-control" type="text" id="playwright">
+                <input class="col-sm-7 form-control" type="text" id="playwright" v-model="formdata.ShowPlaywright" required>
               </div>
               <div class="form-group row">
                 <label class="col-sm-4 form-label">Description:</label>
-                <input class="col-sm-7 form-control" type="text" id="description">
+                <input class="col-sm-7 form-control" type="text" id="description" v-model="formdata.ShowDescription" required>
               </div>
               <div class="form-group row">
                 <label class="col-sm-4 form-label">Show Date:</label>
-                <input class="col-sm-7 form-control" type="date" id="showdate" />
+                <input class="col-sm-7 form-control" type="date" id="showdate" v-model="formdata.ShowDate" required>
               </div>
                 <div class="form-group row">
                 <label class="col-sm-4 form-label">Show Time:</label>
-                 <input class="col-sm-7 form-control" type="time" :value="showtime" id="showtime">
+                 <input class="col-sm-7 form-control" type="time" :value="showtime" id="showtime" required>
               </div>
                 <div class="form-group row">
                   <label class="col-sm-4 form-label">Total Seats:</label>
-                  <input class="col-sm-7 form-control" type="number" id="totalseats">
+                  <input class="col-sm-7 form-control" type="number" id="totalseats" v-model="formdata.NumberOfTickets" required>
                 </div>
                 <div class="form-group row">
                   <label class="col-sm-4 form-label">Venue:</label>
-                  <input class="col-sm-7 form-control" type="text" id="showvenue">
+                  <input class="col-sm-7 form-control" type="text" id="showvenue" v-model="formdata.ShowVenue" required>
                 </div>
                 <div class="form-group row">
                   <label class="col-sm-4 form-label">Show Rating:</label>
-                  <select id="inputState" class="form-control col-sm-7">
+                  <select id="inputState" class="form-control col-sm-7" required v-model="formdata.ShowRating" >
                     <option selected>Choose...</option>
                     <option>A</option>
                     <option>U/A</option>
@@ -59,22 +59,31 @@
                 </div>
                 <div class="form-group row">
                   <label class="col-sm-4 form-label">Adult Price:</label>
-                  <input class="col-sm-7 form-control" type="number" id="adultprice">
+                  <input class="col-sm-7 form-control" type="number" id="adultprice" v-model="formdata.ShowPrice" required>
                 </div>
                 <div class="form-group row">
                   <label class="col-sm-4 form-label">Children Price:</label>
-                  <input class="col-sm-7 form-control" type="number" id="childrenprice">
+                  <input class="col-sm-7 form-control" type="number" id="childrenprice" required>
                 </div>
                 <div class="form-group row">
                   <label class="col-sm-4 form-label">Upload Image:</label>
                   <div class="form-group col-sm-7">
-                    <input type="file" class="form-control-file" id="InputFile" aria-describedby="fileHelp">
+                    <input type="file" class="form-control-file" id="InputFile" aria-describedby="fileHelp" required>
                     <small id="fileHelp" class="form-text text-muted"></small>
                   </div>
                 </div>
-                <button type="submit" class="btn btn-danger">Reset</button>
-                <button type="submit" class="btn btn-success">Add & Save</button>
-            </form>
+                 <div class="form-group row">
+                   <label class="col-sm-4 form-label">Publish:</label>
+              <label class="switch float-right form-group">
+                <input type="checkbox" class="sliderinput" v-model="formdata.isPublished" required>
+                <span class="slider round"></span>
+              </label>
+            </div>
+            <div class="">
+                <button type="reset" class=" btn btn-danger">Reset</button>
+                <button type="submit" class=" btn btn-success">Add & Save</button>
+            </div>
+             </form>
             </div>
           </div>
         </div>
@@ -85,10 +94,22 @@
 <script>
 import AdminShow from './AdminShow.vue'
 import LeftNavbar from './AdminNavbar.vue'
+
 export default {
   name: 'AdminDashboard',
   data () {
     return {
+      formdata: {
+        ShowTitle: '',
+        ShowPlaywright: '',
+        ShowDescription: '',
+        ShowDate: '',
+        NumberOfTickets: 0,
+        ShowVenue: '',
+        ShowRating: '',
+        ShowPrice: 0,
+        isPublished: false
+      },
       /* global moment  */
       showtime: moment().format('HH:mm:ss')
     }
@@ -96,10 +117,82 @@ export default {
   components: {
     AdminShow,
     LeftNavbar
+  },
+  methods: {
+    addshow () {
+      console.log(this.formdata)
+      /* global $ axios */
+      axios.create({
+        baseURL: url,
+        timeout: 1000,
+        headers: {'token': window.localStorage.getItem('AccessToken') }
+      }).post('/addadmin', this.formdata)
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+      $('#myModal').modal('hide')
+    }
   }
 }
 </script>
 
 <style scoped>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
 
+.switch .sliderinput {display:none;}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.sliderinput:checked + .slider {
+  background-color: green;
+}
+
+.sliderinput:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+.sliderinput:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
 </style>
