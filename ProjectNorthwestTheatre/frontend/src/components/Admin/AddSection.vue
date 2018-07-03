f154c5e4738f0dc582f4491f3726fa3c75169071
Supraja Kumbam <s530862@nwmissouri.edu>
2018-07-02 22:21:34 -0500

    Added the AddSection Page

--- ProjectNorthwestTheatre/frontend/src/components/Admin/AddSection.vue
+++ ProjectNorthwestTheatre/frontend/src/components/Admin/AddSection.vue
@@ -1,11 +1,46 @@
 <template>
-  <div>      this is add section page
-      </div>
+<form class="justify-content-center">
+<h1 class="h3 mb-3 font-weight-normal">Add Section</h1>
+<div class="form-group row justify-content-center">
+                <label class="col-sm-2 form-label">Professor Name:</label>
+                <input class="col-sm-4 form-control" type="text" id="profname">
+              </div>
+  <div class="form-group row justify-content-center">
+                  <label class="col-sm-2 form-label">Day:</label>
+                  <select id="inputState" class="col-sm-4 form-control" required v-model="formdata.ShowRating" >
+                    <option selected>Choose...</option>
+                    <option>Monday</option>
+                    <option>Tuesday</option>
+                    <option>Wednesday</option>
+                    <option>Thursday</option>
+                    <option>Friday</option>
+                    <option>Saturday</option>
+                    <option>Sunday</option>
+                  </select>
+                </div>
+  <div class="form-group row justify-content-center">
+                <label class="col-sm-2 form-label">Show Time:</label>
+                 <input class="col-sm-4 form-control" type="time" :value="showtime" id="showtime" required>
+              </div>
+  <div class="form-group row justify-content-center">
+                <label class="col-sm-2 form-label">Section Number:</label>
+                <input class="col-sm-4 form-control" type="text" id="secnumber" >
+              </div>
+  <button type="submit" class="btn btn-success">Add section</button>
+</form>
 </template>
 
 <script>
 export default {
-
+  name: 'AddSection',
+  data () {
+    return {
+      formdata: {
+        ShowRating: '',
+        showtime: ''
+      }
+    }
+  }
 }
 </script>
 <style scoped>
