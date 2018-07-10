974f147618ae86de3bf8d33ee23ba8631835677f
Santhosh Bonala <S530859@nwmissouri.edu>
2018-07-09 21:11:52 -0500

    removed bugs

--- ProjectNorthwestTheatre/frontend/src/components/Admin/AddSection.vue
+++ ProjectNorthwestTheatre/frontend/src/components/Admin/AddSection.vue
@@ -1,13 +1,13 @@
 <template>
-<form class="justify-content-center">
+<form class="justify-content-center" @submit.prevent="addsection" id="addsection">
 <h1 class="h3 mb-3 font-weight-normal">Add Section</h1>
 <div class="form-group row justify-content-center">
                 <label class="col-sm-2 form-label">Professor Name:</label>
-                <input class="col-sm-4 form-control" type="text" id="profname">
+                <input class="col-sm-4 form-control" type="text" id="profname" name="ProfessorName">
               </div>
   <div class="form-group row justify-content-center">
                   <label class="col-sm-2 form-label">Day:</label>
-                  <select id="inputState" class="col-sm-4 form-control" required v-model="formdata.ShowRating" >
+                  <select id="inputState" class="col-sm-4 form-control" name="ClassDay" required v-model="formdata.ShowRating" >
                     <option selected>Choose...</option>
                     <option>Monday</option>
                     <option>Tuesday</option>
@@ -19,12 +19,12 @@
                   </select>
                 </div>
   <div class="form-group row justify-content-center">
-                <label class="col-sm-2 form-label">Show Time:</label>
-                 <input class="col-sm-4 form-control" type="time" :value="showtime" id="showtime" required>
+                <label class="col-sm-2 form-label">Section Time:</label>
+                 <input class="col-sm-4 form-control" type="time"  id="sectiontime" name="ClassTime" required>
               </div>
   <div class="form-group row justify-content-center">
                 <label class="col-sm-2 form-label">Section Number:</label>
-                <input class="col-sm-4 form-control" type="text" id="secnumber" >
+                <input class="col-sm-4 form-control" type="text" id="secnumber" name="SectionNumber">
               </div>
   <button type="submit" class="btn btn-success">Add section</button>
 </form>
@@ -36,9 +36,13 @@ export default {
   data () {
     return {
       formdata: {
-        ShowRating: '',
-        showtime: ''
+        ShowRating: ''
       }
+    }
+  },
+  methods: {
+    addsection () {
+      console.log(new FormData(document.querySelector('#addsection')).get('ClassTime'))
     }
   }
 }
