aada7ce30a74024ac6b4733954eadebe291c433a
Ashwithgundu95 <ashwithgundu@gmail.com>
2018-09-07 09:47:36 -0500

    Added email id

--- ProjectNorthwestTheatre/frontend/src/components/User/UserCancelTicket.vue
+++ ProjectNorthwestTheatre/frontend/src/components/User/UserCancelTicket.vue
@@ -1,10 +1,34 @@
 <template>
 <div>
-  <h1>For cancelling the Tickets:</h1>
-  <h2>Contact:</h2>
-  <h3><b>Patrick Immel</b></h3>  
-  <h3><b>Wendy Eloe</b></h3>
+  
+  <h2></h2>
+ 
+
+<div class="card text-center m-5">
+  <div class="card-header bg-success text-light">
+   <h1>For cancelling the Tickets</h1>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Contact:</h5>
+    <div class="card-text">
+       <h3><b>Patrick Immel : </b><a href="mailto:pimmel@nwmissouri.edu?Subject=Cancel%20Ticket" target="_top">pimmel@nwmissouri.edu</a></h3>  
+  <h3><b>Wendy Eloe : </b><a href="mailto:S524779@nwmissouri.edu?Subject=Cancel%20Ticket" target="_top">S524779@nwmissouri.edu</a> </h3>
+
+    </div>
+    
+  </div>
+  <div class="card-footer text-muted">
+    ....
+  </div>
 </div>
+
+
+
+</div>
+
+
+
+
 </template>
 
 <script>
